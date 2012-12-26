9395b198bc8e972f5eb65fc982f350cd7e65aa02
default_git_name <default_git_email>
2012-12-26 06:29:35 +0000

    Replaced all tabs to four spaces.

--- src/jp/ddo/neko_daisuki/anaudioplayer/MainActivity.java
+++ src/jp/ddo/neko_daisuki/anaudioplayer/MainActivity.java
@@ -26,307 +26,309 @@ import jp.ddo.neko_daisuki.android.widget.UzumakiSlider;
 
 public class MainActivity extends Activity
 {
-	private static final String log_tag = "An Audio Player";
+    private static final String log_tag = "An Audio Player";
 
     @Override
     public void onCreate(Bundle savedInstanceState)
-	{
+    {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main);
 
-		this.initializeFlipButtonListener();
-		this.initializeDirList();
-		this.initializeAnimation();
-		this.initializePlayButton();
+        this.initializeFlipButtonListener();
+        this.initializeDirList();
+        this.initializeAnimation();
+        this.initializePlayButton();
 
-		this.task = new PlayerTask(this);
+        this.task = new PlayerTask(this);
     }
 
-	private void initializePlayButton() {
-		this.play_button = (Button)this.findViewById(R.id.play);
-		this.pause_listener = new PauseButtonListener(this);
-		this.play_button.setOnClickListener(this.pause_listener);
-		this.play_listener = new PlayButtonListener(this);
-	}
+    private void initializePlayButton() {
+        this.play_button = (Button)this.findViewById(R.id.play);
+        this.pause_listener = new PauseButtonListener(this);
+        this.play_button.setOnClickListener(this.pause_listener);
+        this.play_listener = new PlayButtonListener(this);
+    }
 
-	private class ActivityListener {
+    private class ActivityListener {
 
-		public ActivityListener(MainActivity activity) {
-			this.activity = activity;
-		}
+        public ActivityListener(MainActivity activity) {
+            this.activity = activity;
+        }
 
-		protected MainActivity activity;
-	}
+        protected MainActivity activity;
+    }
 
-	private class PauseButtonListener extends ActivityListener implements View.OnClickListener {
+    private class PauseButtonListener extends ActivityListener implements View.OnClickListener {
 
-		public PauseButtonListener(MainActivity activity) {
-			super(activity);
-		}
+        public PauseButtonListener(MainActivity activity) {
+            super(activity);
+        }
 
-		@Override
-		public void onClick(View view) {
-			this.activity.pause();
-		}
-	}
+        @Override
+        public void onClick(View view) {
+            this.activity.pause();
+        }
+    }
 
-	private class PlayButtonListener extends ActivityListener implements View.OnClickListener {
+    private class PlayButtonListener extends ActivityListener implements View.OnClickListener {
 
-		public PlayButtonListener(MainActivity activity) {
-			super(activity);
-		}
+        public PlayButtonListener(MainActivity activity) {
+            super(activity);
+        }
 
-		@Override
-		public void onClick(View view) {
-			this.activity.play();
-		}
-	}
+        @Override
+        public void onClick(View view) {
+            this.activity.play();
+        }
+    }
 
-	private static final long ANIMATION_DURATION = 250;
-	//private static final int INTERPOLATOR = android.R.anim.decelerate_interpolator;
-	private static final int INTERPOLATOR = android.R.anim.linear_interpolator;
+    private static final long ANIMATION_DURATION = 250;
+    //private static final int INTERPOLATOR = android.R.anim.decelerate_interpolator;
+    private static final int INTERPOLATOR = android.R.anim.linear_interpolator;
 
-	private Animation loadAnimation(int id, Interpolator interp) {
-		Animation anim = AnimationUtils.loadAnimation(this, id);
-		anim.setDuration(ANIMATION_DURATION);
-		anim.setInterpolator(interp);
-		return anim;
-	}
+    private Animation loadAnimation(int id, Interpolator interp) {
+        Animation anim = AnimationUtils.loadAnimation(this, id);
+        anim.setDuration(ANIMATION_DURATION);
+        anim.setInterpolator(interp);
+        return anim;
+    }
 
-	private void initializeAnimation() {
-		Interpolator interp = AnimationUtils.loadInterpolator(this, INTERPOLATOR);
-		this.leftInAnimation = this.loadAnimation(R.anim.anim_left_in, interp);
-		this.leftOutAnimation = this.loadAnimation(R.anim.anim_left_out, interp);
-		this.rightInAnimation = this.loadAnimation(R.anim.anim_right_in, interp);
-		this.rightOutAnimation = this.loadAnimation(R.anim.anim_right_out, interp);
-	}
+    private void initializeAnimation() {
+        Interpolator interp = AnimationUtils.loadInterpolator(this, INTERPOLATOR);
+        this.leftInAnimation = this.loadAnimation(R.anim.anim_left_in, interp);
+        this.leftOutAnimation = this.loadAnimation(R.anim.anim_left_out, interp);
+        this.rightInAnimation = this.loadAnimation(R.anim.anim_right_in, interp);
+        this.rightOutAnimation = this.loadAnimation(R.anim.anim_right_out, interp);
+    }
 
-	private static final String MEDIA_PATH = "/sdcard/u1";
+    private static final String MEDIA_PATH = "/sdcard/u1";
 
-	private void initializeDirList() {
-		ListView view = (ListView)this.findViewById(R.id.dir_list);
-		int layout = android.R.layout.simple_list_item_1;
-		this.dirList = this.listMp3Dir(new File(MEDIA_PATH));
-		view.setAdapter(new ArrayAdapter<String>(this, layout, this.dirList));
-		view.setOnItemClickListener(new DirectoryListListener(this));
-	}
+    private void initializeDirList() {
+        ListView view = (ListView)this.findViewById(R.id.dir_list);
+        int layout = android.R.layout.simple_list_item_1;
+        this.dirList = this.listMp3Dir(new File(MEDIA_PATH));
+        view.setAdapter(new ArrayAdapter<String>(this, layout, this.dirList));
+        view.setOnItemClickListener(new DirectoryListListener(this));
+    }
 
-	private File[] listFiles(File dir, FilenameFilter filter) {
-		File[] files;
-		try {
-			files = dir.listFiles(filter);
-		}
-		catch (SecurityException _) {
-			files = null;
-		}
-		return files != null ? files : (new File[0]);
-	}
+    private File[] listFiles(File dir, FilenameFilter filter) {
+        File[] files;
+        try {
+            files = dir.listFiles(filter);
+        }
+        catch (SecurityException _) {
+            files = null;
+        }
+        return files != null ? files : (new File[0]);
+    }
 
-	private List<String> listMp3Dir(File dir) {
-		List<String> list = new ArrayList<String>();
+    private List<String> listMp3Dir(File dir) {
+        List<String> list = new ArrayList<String>();
 
-		for (File d: this.listFiles(dir, new DirectoryFilter())) {
-			list.addAll(this.listMp3Dir(d));
-		}
-		if (0 < this.listFiles(dir, new Mp3Filter()).length) {
-			try {
-				list.add(dir.getCanonicalPath());
-			}
-			catch (IOException _) {
-			}
-		}
+        for (File d: this.listFiles(dir, new DirectoryFilter())) {
+            list.addAll(this.listMp3Dir(d));
+        }
+        if (0 < this.listFiles(dir, new Mp3Filter()).length) {
+            try {
+                list.add(dir.getCanonicalPath());
+            }
+            catch (IOException _) {
+            }
+        }
 
-		return list;
-	}
+        return list;
+    }
 
-	private class Mp3Filter implements FilenameFilter {
+    private class Mp3Filter implements FilenameFilter {
 
-		public boolean accept(File dir, String name) {
-			return name.endsWith(".mp3");
-		}
-	}
+        public boolean accept(File dir, String name) {
+            return name.endsWith(".mp3");
+        }
+    }
 
-	private class DirectoryFilter implements FilenameFilter {
+    private class DirectoryFilter implements FilenameFilter {
 
-		public boolean accept(File dir, String name) {
-			String path;
-			try {
-				path = dir.getCanonicalPath() + File.separator + name;
-			}
-			catch (IOException _) {
-				return false;
-			}
-			return (new File(path)).isDirectory();
-		}
-	}
+        public boolean accept(File dir, String name) {
+            String path;
+            try {
+                path = dir.getCanonicalPath() + File.separator + name;
+            }
+            catch (IOException _) {
+                return false;
+            }
+            return (new File(path)).isDirectory();
+        }
+    }
 
-	private void initializeFlipButtonListener() {
-		this.flipper = (ViewFlipper)this.findViewById(R.id.flipper);
-		int[] next_buttons = { R.id.next0, R.id.next1, R.id.next2 };
-		this.setClickListener(next_buttons, new NextButtonListener(this));
-		int[] previous_buttons = { R.id.prev0, R.id.prev1, R.id.prev2 };
-		this.setClickListener(previous_buttons, new PreviousButtonListener(this));
-	}
+    private void initializeFlipButtonListener() {
+        this.flipper = (ViewFlipper)this.findViewById(R.id.flipper);
+        int[] next_buttons = { R.id.next0, R.id.next1, R.id.next2 };
+        this.setClickListener(next_buttons, new NextButtonListener(this));
+        int[] previous_buttons = { R.id.prev0, R.id.prev1, R.id.prev2 };
+        this.setClickListener(previous_buttons, new PreviousButtonListener(this));
+    }
 
-	private void selectDir(int position) {
-		int layout = android.R.layout.simple_list_item_1;
-		String dir = this.dirList.get(position);
-		this.files = (new File(dir)).list(new Mp3Filter());
+    private void selectDir(int position) {
+        int layout = android.R.layout.simple_list_item_1;
+        String dir = this.dirList.get(position);
+        this.files = (new File(dir)).list(new Mp3Filter());
 
-		ListView view = (ListView)this.findViewById(R.id.file_list);
-		view.setAdapter(new ArrayAdapter<String>(this, layout, this.files));
-		view.setOnItemClickListener(new FileListListener(this));
+        ListView view = (ListView)this.findViewById(R.id.file_list);
+        view.setAdapter(new ArrayAdapter<String>(this, layout, this.files));
+        view.setOnItemClickListener(new FileListListener(this));
 
-		this.showNext();
-	}
+        this.showNext();
+    }
 
-	private void pause() {
-		this.timer.cancel();
-		this.play_button.setOnClickListener(this.play_listener);
-	}
+    private void pause() {
+        this.timer.cancel();
+        this.play_button.setOnClickListener(this.play_listener);
+    }
 
-	private class PlayerTask extends TimerTask {
+    private class PlayerTask extends TimerTask {
 
-		public PlayerTask(MainActivity activity) {
-			this.handler = new Handler();
-			this.proc = new Proc(activity);
-		}
+        public PlayerTask(MainActivity activity) {
+            this.handler = new Handler();
+            this.proc = new Proc(activity);
+        }
 
-		public class Proc implements Runnable {
+        public class Proc implements Runnable {
 
-			public Proc(MainActivity activity) {
-				this.activity = activity;
-			}
+            public Proc(MainActivity activity) {
+                this.activity = activity;
+            }
 
-			public void run() {
-				this.activity.updateSlider();
-			}
+            public void run() {
+                this.activity.updateSlider();
+            }
 
-			private MainActivity activity;
-		}
+            private MainActivity activity;
+        }
 
-		@Override
-		public void run() {
-			this.handler.post(this.proc);
-		}
+        @Override
+        public void run() {
+            this.handler.post(this.proc);
+        }
 
-		private Handler handler;
-		private Runnable proc;
-	}
+        private Handler handler;
+        private Runnable proc;
+    }
 
-	private void updateSlider() {
-		UzumakiSlider slider = (UzumakiSlider)this.findViewById(R.id.slider);
-		slider.setProgress(slider.getProgress() + 1);
-	}
+    private void updateSlider() {
+        UzumakiSlider slider = (UzumakiSlider)this.findViewById(R.id.slider);
+        slider.setProgress(slider.getProgress() + 1);
+    }
 
-	private void play() {
-		this.timer = new Timer(true);
-		this.timer.scheduleAtFixedRate(this.task, 0, 10);
+    private void play() {
+        this.timer = new Timer(true);
+        this.timer.scheduleAtFixedRate(this.task, 0, 10);
 
-		this.play_button.setOnClickListener(this.pause_listener);
-	}
+        this.play_button.setOnClickListener(this.pause_listener);
+    }
 
-	private void selectFile(int position) {
-		this.filePosition = position;
-		this.showNext();
-		this.play();
-	}
+    private void selectFile(int position) {
+        this.filePosition = position;
+        this.showNext();
+        this.play();
+    }
 
-	private void showPrevious() {
-		this.flipper.setInAnimation(this.leftInAnimation);
-		this.flipper.setOutAnimation(this.rightOutAnimation);
-		this.flipper.showPrevious();
-	}
+    private void showPrevious() {
+        this.flipper.setInAnimation(this.leftInAnimation);
+        this.flipper.setOutAnimation(this.rightOutAnimation);
+        this.flipper.showPrevious();
+    }
 
-	private void showNext() {
-		this.flipper.setInAnimation(this.rightInAnimation);
-		this.flipper.setOutAnimation(this.leftOutAnimation);
-		this.flipper.showNext();
-	}
+    private void showNext() {
+        this.flipper.setInAnimation(this.rightInAnimation);
+        this.flipper.setOutAnimation(this.leftOutAnimation);
+        this.flipper.showNext();
+    }
 
-	private abstract class ListListener implements AdapterView.OnItemClickListener {
+    private abstract class ListListener implements AdapterView.OnItemClickListener {
 
-		public ListListener(MainActivity activity) {
-			this.activity = activity;
-		}
+        public ListListener(MainActivity activity) {
+            this.activity = activity;
+        }
 
-		protected MainActivity activity;
-	}
+        protected MainActivity activity;
+    }
 
-	private class DirectoryListListener extends ListListener {
+    private class DirectoryListListener extends ListListener {
 
-		public DirectoryListListener(MainActivity activity) {
-			super(activity);
-		}
+        public DirectoryListListener(MainActivity activity) {
+            super(activity);
+        }
 
-		public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-			this.activity.selectDir(position);
-		}
-	}
+        public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+            this.activity.selectDir(position);
+        }
+    }
 
-	private class FileListListener extends ListListener {
+    private class FileListListener extends ListListener {
 
-		public FileListListener(MainActivity activity) {
-			super(activity);
-		}
+        public FileListListener(MainActivity activity) {
+            super(activity);
+        }
 
-		public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-			this.activity.selectFile(position);
-		}
-	}
+        public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+            this.activity.selectFile(position);
+        }
+    }
 
-	private void setClickListener(int[] widgets, View.OnClickListener listener) {
-		for (int id: widgets) {
-			Button button = (Button)this.findViewById(id);
-			button.setOnClickListener(listener);
-		}
-	}
+    private void setClickListener(int[] widgets, View.OnClickListener listener) {
+        for (int id: widgets) {
+            Button button = (Button)this.findViewById(id);
+            button.setOnClickListener(listener);
+        }
+    }
 
-	private abstract class FlipButtonListener implements View.OnClickListener {
+    private abstract class FlipButtonListener implements View.OnClickListener {
 
-		public FlipButtonListener(MainActivity activity) {
-			this.activity = activity;
-		}
+        public FlipButtonListener(MainActivity activity) {
+            this.activity = activity;
+        }
 
-		protected MainActivity activity;
-	}
+        protected MainActivity activity;
+    }
 
-	private class NextButtonListener extends FlipButtonListener {
+    private class NextButtonListener extends FlipButtonListener {
 
-		public NextButtonListener(MainActivity activity) {
-			super(activity);
-		}
+        public NextButtonListener(MainActivity activity) {
+            super(activity);
+        }
 
-		@Override
-		public void onClick(View view) {
-			this.activity.showNext();
-		}
-	}
+        @Override
+        public void onClick(View view) {
+            this.activity.showNext();
+        }
+    }
 
-	private class PreviousButtonListener extends FlipButtonListener {
+    private class PreviousButtonListener extends FlipButtonListener {
 
-		public PreviousButtonListener(MainActivity activity) {
-			super(activity);
-		}
+        public PreviousButtonListener(MainActivity activity) {
+            super(activity);
+        }
 
-		@Override
-		public void onClick(View view) {
-			this.activity.showPrevious();
-		}
-	}
+        @Override
+        public void onClick(View view) {
+            this.activity.showPrevious();
+        }
+    }
 
-	private List<String> dirList = null;
-	private String[] files = new String[0];
-	private ViewFlipper flipper;
-	private Animation leftInAnimation;
-	private Animation leftOutAnimation;
-	private Animation rightInAnimation;
-	private Animation rightOutAnimation;
-	private int filePosition;
+    private List<String> dirList = null;
+    private String[] files = new String[0];
+    private ViewFlipper flipper;
+    private Animation leftInAnimation;
+    private Animation leftOutAnimation;
+    private Animation rightInAnimation;
+    private Animation rightOutAnimation;
+    private int filePosition;
 
-	private Button play_button;
-	private View.OnClickListener pause_listener;
-	private View.OnClickListener play_listener;
-	private Timer timer;
-	private PlayerTask task;
+    private Button play_button;
+    private View.OnClickListener pause_listener;
+    private View.OnClickListener play_listener;
+    private Timer timer;
+    private PlayerTask task;
 }
+
+// vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
--- src/jp/ddo/neko_daisuki/android/widget/UzumakiDiagram.java
+++ src/jp/ddo/neko_daisuki/android/widget/UzumakiDiagram.java
@@ -7,77 +7,79 @@ import android.graphics.RectF;
 
 public class UzumakiDiagram
 {
-	private int start_angle;
-	private int sweep_angle;
-	private int outer_diameter;
-	private int inner_diameter;
-	private Paint paint;
+    private int start_angle;
+    private int sweep_angle;
+    private int outer_diameter;
+    private int inner_diameter;
+    private Paint paint;
 
-	public UzumakiDiagram(int start_angle, int sweep_angle, int outer_diameter, int inner_diameter, Paint paint) {
-		this.start_angle = start_angle;
-		this.sweep_angle = sweep_angle;
-		this.outer_diameter = outer_diameter;
-		this.inner_diameter = inner_diameter;
-		this.paint = paint;
-	}
+    public UzumakiDiagram(int start_angle, int sweep_angle, int outer_diameter, int inner_diameter, Paint paint) {
+        this.start_angle = start_angle;
+        this.sweep_angle = sweep_angle;
+        this.outer_diameter = outer_diameter;
+        this.inner_diameter = inner_diameter;
+        this.paint = paint;
+    }
 
-	public void draw(Canvas canvas) {
-		int outer_diameter = this.outer_diameter == 0 ? Math.min(canvas.getWidth(), canvas.getHeight()) : this.outer_diameter;
-		this.draw(canvas, outer_diameter, this.inner_diameter);
-	}
+    public void draw(Canvas canvas) {
+        int outer_diameter = this.outer_diameter == 0 ? Math.min(canvas.getWidth(), canvas.getHeight()) : this.outer_diameter;
+        this.draw(canvas, outer_diameter, this.inner_diameter);
+    }
 
-	private void draw(Canvas canvas, int outer_diameter, int inner_diameter) {
-		int left = (canvas.getWidth() - outer_diameter) / 2;
-		int top = (canvas.getHeight() - outer_diameter) / 2;
-		RectF oval = new RectF(left, top, left + outer_diameter, top + outer_diameter);
+    private void draw(Canvas canvas, int outer_diameter, int inner_diameter) {
+        int left = (canvas.getWidth() - outer_diameter) / 2;
+        int top = (canvas.getHeight() - outer_diameter) / 2;
+        RectF oval = new RectF(left, top, left + outer_diameter, top + outer_diameter);
 
-		Path path = new Path();
-		int diameter_step = (outer_diameter - inner_diameter) / (Math.abs(this.sweep_angle) / 180) / 2;
-		RectShrinker verticalShrinker = new VerticalShrinker(diameter_step);
-		RectShrinker horizontalShrinker = new HorizontalShrinker(diameter_step);
-		int angle_step = (this.sweep_angle < 0 ? -1 : 1) * 90;
-		int angle;
-		for (angle = this.start_angle; angle != this.sweep_angle; angle += angle_step) {
-			RectShrinker shrinker = angle % 180 == 0 ? verticalShrinker : horizontalShrinker;
-			shrinker.shrink(oval);
-			path.addArc(oval, angle, angle_step);
-		}
+        Path path = new Path();
+        int diameter_step = (outer_diameter - inner_diameter) / (Math.abs(this.sweep_angle) / 180) / 2;
+        RectShrinker verticalShrinker = new VerticalShrinker(diameter_step);
+        RectShrinker horizontalShrinker = new HorizontalShrinker(diameter_step);
+        int angle_step = (this.sweep_angle < 0 ? -1 : 1) * 90;
+        int angle;
+        for (angle = this.start_angle; angle != this.sweep_angle; angle += angle_step) {
+            RectShrinker shrinker = angle % 180 == 0 ? verticalShrinker : horizontalShrinker;
+            shrinker.shrink(oval);
+            path.addArc(oval, angle, angle_step);
+        }
 
-		canvas.drawPath(path, this.paint);
-	}
+        canvas.drawPath(path, this.paint);
+    }
 
-	private abstract class RectShrinker {
+    private abstract class RectShrinker {
 
-		public RectShrinker(int size) {
-			this.size = size;
-		}
+        public RectShrinker(int size) {
+            this.size = size;
+        }
 
-		public abstract void shrink(RectF rect);
+        public abstract void shrink(RectF rect);
 
-		protected int size;
-	}
+        protected int size;
+    }
 
-	private class HorizontalShrinker extends RectShrinker {
+    private class HorizontalShrinker extends RectShrinker {
 
-		public HorizontalShrinker(int size) {
-			super(size);
-		}
+        public HorizontalShrinker(int size) {
+            super(size);
+        }
 
-		public void shrink(RectF rect) {
-			rect.left += this.size;
-			rect.right -= this.size;
-		}
-	}
+        public void shrink(RectF rect) {
+            rect.left += this.size;
+            rect.right -= this.size;
+        }
+    }
 
-	private class VerticalShrinker extends RectShrinker {
+    private class VerticalShrinker extends RectShrinker {
 
-		public VerticalShrinker(int size) {
-			super(size);
-		}
+        public VerticalShrinker(int size) {
+            super(size);
+        }
 
-		public void shrink(RectF rect) {
-			rect.bottom -= this.size;
-			rect.top += this.size;
-		}
-	}
+        public void shrink(RectF rect) {
+            rect.bottom -= this.size;
+            rect.top += this.size;
+        }
+    }
 }
+
+// vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
