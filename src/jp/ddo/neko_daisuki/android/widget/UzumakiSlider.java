23215af5aca6843e55a3918c569fcb8e7bb4dd50
default_git_name <default_git_email>
2012-12-29 16:35:09 +0000

    Enhanced UzumakiSlider to accept inner_diameter with %.

--- src/jp/ddo/neko_daisuki/android/widget/UzumakiSlider.java
+++ src/jp/ddo/neko_daisuki/android/widget/UzumakiSlider.java
@@ -6,6 +6,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Region;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public abstract class UzumakiSlider extends View {
@@ -42,13 +43,51 @@ public abstract class UzumakiSlider extends View {
         this.progress = 0;
     }
 
+    private abstract class MemberSetter {
+
+        public abstract void set(UzumakiSlider slider, int value);
+    }
+
+    public void setInnerDiameter(int value) {
+        this.inner_diameter = value;
+    }
+
+    public void setInnerDiameterType(SizeType type) {
+        this.inner_diameter_type = type;
+    }
+
+    private class InnerDiameterPercentSetter extends MemberSetter {
+
+        public void set(UzumakiSlider slider, int value) {
+            slider.setInnerDiameter(value);
+            slider.setInnerDiameterType(SizeType.TYPE_PERCENT);
+        }
+    }
+
+    private class InnerDiameterPixelSetter extends MemberSetter {
+
+        public void set(UzumakiSlider slider, int value) {
+            slider.setInnerDiameter(value);
+            slider.setInnerDiameterType(SizeType.TYPE_PIXEL);
+        }
+    }
+
+    private void parseSize(String value, MemberSetter percentSetter, MemberSetter pixelSetter) {
+        boolean isPercent = value.endsWith("%");
+        int n = Integer.parseInt(isPercent ? value.substring(0, value.length() - 1) : value);
+        MemberSetter setter = isPercent ? percentSetter : pixelSetter;
+        setter.set(this, n);
+    }
+
     private static final int DEFAULT_SWEEP_ANGLE = -3 * 360;
 
     private void readAttribute(AttributeSet attrs) {
         this.start_angle = attrs.getAttributeIntValue(null, "start_angle", 0);
         this.sweep_angle = attrs.getAttributeIntValue(null, "sweep_angle", DEFAULT_SWEEP_ANGLE);
         this.outer_diameter = attrs.getAttributeIntValue(null, "outer_diameter", 0);
-        this.inner_diameter = attrs.getAttributeIntValue(null, "inner_diameter", 0);
+
+        this.parseSize(attrs.getAttributeValue(null, "inner_diameter"), new InnerDiameterPercentSetter(), new InnerDiameterPixelSetter());
+
         this.outline_outer_diameter = attrs.getAttributeIntValue(null, "outline_outer_diameter", this.outer_diameter);
         this.outline_inner_diameter = attrs.getAttributeIntValue(null, "outline_inner_diameter", this.inner_diameter);
     }
@@ -60,8 +99,9 @@ public abstract class UzumakiSlider extends View {
         paint.setStrokeWidth(10);
         paint.setStyle(Paint.Style.STROKE);
 
-        int outer_diameter = this.outer_diameter != 0 ? this.outer_diameter : Math.min(Math.min(x, this.getWidth() - x), Math.min(y, this.getHeight() - y));
-        UzumakiDiagram uzumaki = new UzumakiDiagram(x, y, this.start_angle, this.sweep_angle, outer_diameter, this.inner_diameter, paint);
+        int outer_diameter = this.outer_diameter != 0 ? this.outer_diameter : (2 * Math.min(Math.min(x, this.getWidth() - x), Math.min(y, this.getHeight() - y)));
+        int inner_diameter = this.inner_diameter_type == SizeType.TYPE_PERCENT ? (outer_diameter * this.inner_diameter) / 100 : this.inner_diameter;
+        UzumakiDiagram uzumaki = new UzumakiDiagram(x, y, this.start_angle, this.sweep_angle, outer_diameter, inner_diameter, paint);
         uzumaki.draw(canvas);
     }
 
@@ -92,12 +132,17 @@ public abstract class UzumakiSlider extends View {
     private int max;
     private int progress;
 
+    private enum SizeType { TYPE_PIXEL, TYPE_PERCENT };
+
     private int start_angle;
     private int sweep_angle;
     private int outer_diameter;
     private int inner_diameter;
+    private SizeType inner_diameter_type;
     private int outline_outer_diameter;
+    private SizeType outline_outer_diameter_type;
     private int outline_inner_diameter;
+    private SizeType outline_inner_diameter_type;
 }
 
 // vim: tabstop=4 shiftwidth=4 expandtab softtabstop=4
