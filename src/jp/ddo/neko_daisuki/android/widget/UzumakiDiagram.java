98fde6d115208a1838d6b9a6339b6fd43f1de563
default_git_name <default_git_email>
2013-01-02 13:32:02 +0000

    Fixed uzumaki'shape.
    I changed to use the most simple way.

--- src/jp/ddo/neko_daisuki/android/widget/UzumakiDiagram.java
+++ src/jp/ddo/neko_daisuki/android/widget/UzumakiDiagram.java
@@ -7,17 +7,17 @@ import android.graphics.RectF;
 
 public class UzumakiDiagram
 {
-    private int x;
-    private int y;
+    private int centerX;
+    private int centerY;
     private int startAngle;
     private int sweepAngle;
     private int outerDiameter;
     private int innerDiameter;
     private Paint paint;
 
-    public UzumakiDiagram(int x, int y, int startAngle, int sweepAngle, int outerDiameter, int innerDiameter, Paint paint) {
-        this.x = x;
-        this.y = y;
+    public UzumakiDiagram(int centerX, int centerY, int startAngle, int sweepAngle, int outerDiameter, int innerDiameter, Paint paint) {
+        this.centerX = centerX;
+        this.centerY = centerY;
         this.startAngle = startAngle;
         this.sweepAngle = sweepAngle;
         this.outerDiameter = outerDiameter;
@@ -26,60 +26,50 @@ public class UzumakiDiagram
     }
 
     public void draw(Canvas canvas) {
-        int outerDiameter = this.outerDiameter;
-        int innerDiameter = this.innerDiameter;
-        int left = this.x - outerDiameter / 2;
-        int top = this.y - outerDiameter / 2;
-        RectF oval = new RectF(left, top, left + outerDiameter, top + outerDiameter);
-
+        Point p = new Point();
         Path path = new Path();
-        int diameterStep = (outerDiameter - innerDiameter) / (Math.abs(this.sweepAngle) / 180) / 2;
-        RectShrinker verticalShrinker = new VerticalShrinker(diameterStep);
-        RectShrinker horizontalShrinker = new HorizontalShrinker(diameterStep);
-        int angleStep = (this.sweepAngle < 0 ? -1 : 1) * 90;
-        int angle;
-        for (angle = this.startAngle; angle != this.sweepAngle; angle += angleStep) {
-            RectShrinker shrinker = angle % 180 == 0 ? verticalShrinker : horizontalShrinker;
-            shrinker.shrink(oval);
-            path.addArc(oval, angle, angleStep);
+        this.computePoint(p, this.startAngle);
+        path.moveTo(p.x, p.y);
+
+        int sweepAngle = Math.abs(this.sweepAngle);
+        /*
+         * About "resolution"
+         * ==================
+         *
+         * I'm using Acer A500. Its screen is 10inch (25.4cm) and 1270x800pixel.
+         * So it is 0.17mm/pixel. If I draw one circle as large as possible
+         * (in other words, the circle's radius is 400pixel), the length of its
+         * outline is about 2,500pixel (because of 2 * pi * radius). The length
+         * for one degree is about 7pixel (2,500 / 360), it will be 2mm
+         * (7pixel * 0.17mm/pixel).
+         *
+         * The outline length of four degrees is about 8mm. I felt that it is
+         * smooth enough.
+         */
+        int resolution = 4;
+        int direction = 0 < this.sweepAngle ? 1 : -1;
+        for (int angle = 0; angle < sweepAngle; angle += resolution) {
+            this.computePoint(p, this.startAngle + direction * angle);
+            path.lineTo(p.x, p.y);
         }
 
         canvas.drawPath(path, this.paint);
     }
 
-    private abstract class RectShrinker {
-
-        public RectShrinker(int size) {
-            this.size = size;
-        }
-
-        public abstract void shrink(RectF rect);
-
-        protected int size;
+    private void computePoint(Point dest, int angle) {
+        int diameterDelta = this.outerDiameter - this.innerDiameter;
+        float ratio = (float)(this.startAngle - angle) / Math.abs(this.sweepAngle);
+        float radius = (this.outerDiameter - diameterDelta * ratio) / 2;
+        float x = radius * (float)Math.cos(Math.toRadians(angle));
+        float y = radius * (float)Math.sin(Math.toRadians(angle));
+        dest.x = centerX + x;
+        dest.y = centerY + y;
     }
 
-    private class HorizontalShrinker extends RectShrinker {
+    private class Point {
 
-        public HorizontalShrinker(int size) {
-            super(size);
-        }
-
-        public void shrink(RectF rect) {
-            rect.left += this.size;
-            rect.right -= this.size;
-        }
-    }
-
-    private class VerticalShrinker extends RectShrinker {
-
-        public VerticalShrinker(int size) {
-            super(size);
-        }
-
-        public void shrink(RectF rect) {
-            rect.bottom -= this.size;
-            rect.top += this.size;
-        }
+        public float x = 0.0f;
+        public float y = 0.0f;
     }
 }
 
