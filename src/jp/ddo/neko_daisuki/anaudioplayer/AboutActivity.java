4ab249c9a330c4073a32b4d1ea67ac5c4f9359c4
default_git_name <default_git_email>
2013-01-11 05:11:51 +0000

    Enhanced AboutActivity to see version number at runtime.

--- src/jp/ddo/neko_daisuki/anaudioplayer/AboutActivity.java
+++ src/jp/ddo/neko_daisuki/anaudioplayer/AboutActivity.java
@@ -1,7 +1,10 @@
 package jp.ddo.neko_daisuki.anaudioplayer;
 
 import android.app.Activity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends Activity {
 
@@ -9,6 +12,25 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.about);
+        this.showVersion();
+    }
+
+    private void showVersion() {
+        PackageManager pm = this.getPackageManager();
+        String name = this.getPackageName();
+        int flags = PackageManager.GET_INSTRUMENTATION;
+
+        PackageInfo pi;
+        try {
+            pi = pm.getPackageInfo(name, flags);
+        }
+        catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        TextView view = (TextView)this.findViewById(R.id.version);
+        view.setText(pi.versionName);
     }
 }
 
