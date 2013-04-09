a98aca01ca20335a253586297e567fc42f3e63a7
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-04-10 03:06:13 +0900

    Removed files for TerminalIDE.

--- run.sh
+++
@@ -1,8 +0,0 @@
-#Script to start the Andoid app
-
-. projectrc
-
-pkg=$(echo ${pkg_dir} | sed -e s%/%.%g)
-
-#'am' is found on most/all devices.. i hope..
-am start -a android.intent.action.MAIN -n ${pkg}/.MainActivity
