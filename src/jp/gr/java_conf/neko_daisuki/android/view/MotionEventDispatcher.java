a37b50bd5c5d491cc697cca4fabd1f1972f2e645
Tomohiko Sumi <SumiTomohiko@neko-daisuki.ddo.jp>
2013-11-16 20:06:26 +0900

    Refactored to use SparseArray.

--- src/jp/gr/java_conf/neko_daisuki/android/view/MotionEventDispatcher.java
+++ src/jp/gr/java_conf/neko_daisuki/android/view/MotionEventDispatcher.java
@@ -1,8 +1,6 @@
 package jp.gr.java_conf.neko_daisuki.android.view;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import android.util.SparseArray;
 import android.view.MotionEvent;
 
 public class MotionEventDispatcher {
@@ -19,51 +17,51 @@ public class MotionEventDispatcher {
         }
     }
 
-    private Map<Integer, Proc> map;
+    private SparseArray<Proc> mMap;
 
     public MotionEventDispatcher() {
-        this.map = new HashMap<Integer, Proc>();
+        mMap = new SparseArray<Proc>();
     }
 
     public void setDownProc(Proc proc) {
-        this.setProc(MotionEvent.ACTION_DOWN, proc);
+        setProc(MotionEvent.ACTION_DOWN, proc);
     }
 
     public void setUpProc(Proc proc) {
-        this.setProc(MotionEvent.ACTION_UP, proc);
+        setProc(MotionEvent.ACTION_UP, proc);
     }
 
     public void setMoveProc(Proc proc) {
-        this.setProc(MotionEvent.ACTION_MOVE, proc);
+        setProc(MotionEvent.ACTION_MOVE, proc);
     }
 
     public void removeDownProc() {
-        this.removeProc(MotionEvent.ACTION_DOWN);
+        removeProc(MotionEvent.ACTION_DOWN);
     }
 
     public void removeUpProc() {
-        this.removeProc(MotionEvent.ACTION_UP);
+        removeProc(MotionEvent.ACTION_UP);
     }
 
     public void removeMoveProc() {
-        this.removeProc(MotionEvent.ACTION_MOVE);
+        removeProc(MotionEvent.ACTION_MOVE);
     }
 
     public boolean dispatch(MotionEvent event) {
-        return this.getProc(event.getActionMasked()).run(event);
+        return getProc(event.getActionMasked()).run(event);
     }
 
     private Proc getProc(int action) {
-        Proc proc = this.map.get(action);
+        Proc proc = mMap.get(action);
         return proc != null ? proc : new FakeProc();
     }
 
     private void setProc(int action, Proc proc) {
-        this.map.put(action, proc);
+        mMap.put(action, proc);
     }
 
     private void removeProc(int action) {
-        this.map.remove(action);
+        mMap.remove(action);
     }
 }
 
